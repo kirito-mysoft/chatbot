2fa8da850c17247ae7d4e6ec0c5df705f84d1fa3
mostakimjihad <jihad.mysoftheaven@gmail.com>
2024-08-25 14:29:00 +0600

    alibaba embeding model added and language detector included

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
-import spacy
 from flask_cors import CORS
+from langdetect import detect, DetectorFactory
+from langdetect.lang_detect_exception import LangDetectException
+
+DetectorFactory.seed = 0
 
 app = Flask(__name__)
 CORS(app)
 
-nlp = spacy.load("en_core_web_sm")
-sentence_model = SentenceTransformer('all-MiniLM-L6-v2')
+sentence_model = SentenceTransformer("Alibaba-NLP/gte-multilingual-base", trust_remote_code=True)
 
 def load_knowledge_base(file_path):
     knowledge_base = {}
@@ -22,6 +24,12 @@ def log_fallback_question(user_input, log_file='fallback_questions.txt'):
     with open(log_file, 'a', encoding='utf-8') as file:
         file.write(f"{user_input}\n")
 
+def detect_language(text):
+    try:
+        lang = detect(text)
+        return lang
+    except LangDetectException:
+        return None
 
 def find_best_match(user_input, knowledge_base):
     questions = list(knowledge_base.keys())
@@ -33,7 +41,7 @@ def find_best_match(user_input, knowledge_base):
     similarity_scores = cosine_similarity(user_input_embedding, question_embeddings)
     best_match_idx = similarity_scores.argmax()
     
-    if similarity_scores.max() > 0.80:
+    if similarity_scores.max() > 0.85:
         return answers[best_match_idx]
     else:
         log_fallback_question(user_input)  
@@ -41,14 +49,19 @@ def find_best_match(user_input, knowledge_base):
 
 knowledge_base = load_knowledge_base('knowledge_base.txt')
 
-
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.json
     user_input = data.get('user_input', '')
     
     if not user_input:
-        return jsonify({"error": "No input provided"}), 400
+        return jsonify({"user_input": "প্রশ্ন ঘরটি খালি রেখেছেন। তাই উত্তর দেয়া সম্ভব নয়।"}), 400
+    
+    # Detect language
+    lang = detect_language(user_input)
+    
+    if lang != 'bn':
+        return jsonify({"user_input": user_input, "response": "বাংলায় কথা বলুন"}), 400
     
     response = find_best_match(user_input, knowledge_base)
     return jsonify({"user_input": user_input, "response": response})
